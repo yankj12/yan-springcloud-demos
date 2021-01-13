51f921869a460811eb7d8ea5754e0f71aebf50f4
yankj12 <yankj12@163.com>
2021-01-13 14:56:36 +0800

    集成Hystrix熔断器

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 SpringCloud的demo
 
+
+|微服务名称|端口|备注|
+|---|---|---|
+|yan-springcloud-eurekaserver|8000|服务注册发现，eureka服务端|
+|yan-springcloud-echo|9000|服务提供方|
+|yan-springcloud-hello|9001|服务调用方，集成了断路器|
+
+
 ## 目录
 
 [eureka服务器端](./yan-springcloud-eurekaserver/README.md)
--- yan-springcloud-hello/src/main/java/com/yan/springcloud/hello/service/HelloRemote.java
+++ yan-springcloud-hello/src/main/java/com/yan/springcloud/hello/service/HelloRemote.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "yan-springcloud-echo")
+@FeignClient(name= "yan-springcloud-echo", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
